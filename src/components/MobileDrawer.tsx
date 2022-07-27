d98b32dab299bceb9c6a2fd3e9af569f90d3c37b
yousaf wazir <umbrellaman612@gmail.com>
2022-07-27 16:06:52 +0100

    code: added drawer options

--- src/components/MobileDrawer.tsx
+++ src/components/MobileDrawer.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
+
 import { useRecoilValue } from 'recoil'
 
 import { mobileDrawerState } from '../../atoms'
 import { SearchInput } from './SearchInput'
 
 interface MobileDrawerProps {}
+interface DrawerOptionProps {
+  option: string
+}
 
 export const MobileDrawer: React.FC<MobileDrawerProps> = () => {
   const showDrawer = useRecoilValue(mobileDrawerState)
@@ -11,14 +16,48 @@ export const MobileDrawer: React.FC<MobileDrawerProps> = () => {
     <div
       className={`${
         showDrawer
-          ? 'h-[500px] scale-y-100 flex bg-[#161B22] border px-3 py-2 text-[#BABBBD]'
+          ? 'h-[450px] scale-y-100 flex bg-[#161B22] border px-3 py-2 flex-col'
           : 'hidden'
       }`}
     >
       {/** Search Input */}
-      <div className='w-full h-[45px] flex items-center justify-center'>
+      <div className="w-full h-[45px] flex items-center justify-center mb-4">
         <SearchInput />
+      </div>
+
+      {/** Drawer Options */}
+
+      <div className="h-full">
+        {Options.map((option) => (
+          <DrawerOption option={option} key={option} />
+        ))}
+
+        {/** User */}
       </div>
     </div>
   )
 }
+
+const DrawerOption: React.FC<DrawerOptionProps> = ({ option }) => {
+  return (
+    <Link href={`/${option}`}>
+      <a>
+        <div className="border-t-[0.4px] text-md flex items-center h-[40px] cursor-pointer group border-[#3A3D43]">
+          <h6 className="group-hover:opacity-90 transition-all ease-in-out text-[#F0F6FC] group-hover:text-[#BABBBD] font-medium tracking-wide">
+            {option}
+          </h6>
+        </div>
+      </a>
+    </Link>
+  )
+}
+const Options = [
+  'Dashboard',
+  'Pull requests',
+  'Issues',
+  'Marketplace',
+  'Explore',
+  'Codespaces',
+  'Sponsors',
+  'Settings',
+]
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,7 +4,7 @@ export const SearchInput: React.FC<SearchInputProps> = () => {
   return (
     <input
       type="text"
-      className="w-full transition-all ease-in-out bg-[#0D1116] h-full px-4 rounded-md shadow-xl text-[#C3C3C5] border-[1px] outline-[0.5px] border-[#30363D] outline tracking-wide"
+      className="w-full transition-all ease-in-out bg-[#0D1116] h-full px-4 rounded-md shadow-xl text-[#C3C3C5] border-[1px] outline-[0.5px] border-[#30363D] outline-none tracking-wide"
       placeholder="Search or jump to..."
     />
   )
