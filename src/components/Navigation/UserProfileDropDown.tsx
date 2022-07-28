5affd66e5a9267bdcd6ed8bf41fb3d510e0c81a8
yousaf wazir <umbrellaman612@gmail.com>
2022-07-28 14:51:31 +0100

    feat: added set status button

--- src/components/Navigation/UserProfileDropDown.tsx
+++ src/components/Navigation/UserProfileDropDown.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
+import Link from 'next/link';
+
+
 
 import { useState } from 'react'
-import { MdExpandMore } from 'react-icons/md'
+import { BsEmojiSmile } from 'react-icons/bs'
+import { MdExpandMore } from 'react-icons/md';
+
 
 interface UserProfileDropDownProps {}
 
@@ -41,6 +45,15 @@ export const UserProfileDropDown: React.FC<UserProfileDropDownProps> = () => {
             </div>
           </a>
         </Link>
+
+        {/** Set status */}
+
+        <div className="border-t-[0.5px] border-b-[0.5px] h-[45px] flex items-center justify-center border-[#393f47] mt-2">
+          <button className="flex items-center justify-start border-[0.5px] py-1 w-[150px] px-2 rounded-md border-[#31383f] shadow-md group bg-[#1a2028]">
+            <BsEmojiSmile className="mr-2 text-sm text-gray-500 group-hover:text-blue-500" />
+            <p className="text-sm text-gray-500 font-base group-hover:text-blue-500">Set status</p>
+          </button>
+        </div>
       </div>
     </div>
   )
