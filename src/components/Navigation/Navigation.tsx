af0cd1190463a68d81b0c7d75a682e9c0fdf0ba5
yousaf wazir <umbrellaman612@gmail.com>
2022-07-29 17:52:05 +0100

    fix: removed deleted component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 
 import { Logo } from '../Logo'
-import { MobileMenuHandler } from '../MobileView'
 import { SearchInput } from '../SearchInput'
 import { NavigationGroup } from './NavigationGroup'
 
@@ -12,7 +11,7 @@ export const Navigation: React.FC<NavigationProps> = () => {
   return (
     <header>
       <nav className="max-w-screen-2xl mx-auto h-[85px] flex items-center justify-between px-3 bg-[#161B22] md:h-[70px] sticky top-0 z-10">
-        <MobileMenuHandler />
+     
 
         {/** ====== Left side ====== */}
 
