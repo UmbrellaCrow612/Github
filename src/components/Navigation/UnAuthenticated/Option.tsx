c5e6f93b41313a7c9190ebbce2ac1f2c3b1a7157
Yousaf Wazir <107571370+UmbrellaCrow612@users.noreply.github.com>
2022-08-02 14:39:26 +0100

    Development (#40)
    * style: bg col added to header to span full screen
    
    * style: changed folder name
    
    * style: removed the sticky header
    
    * feat: redirecting un-auth user to home
    
    * code: added the home components folder
    
    * code: added meta title and des
    
    * style: un-auth header max-w reduced
    
    * feat: added the favicon to head
    
    * feat: added close option on mouse away

--- src/components/Navigation/UnAuthenticated/Option.tsx
+++ src/components/Navigation/UnAuthenticated/Option.tsx
@@ -74,6 +74,7 @@ export const Option: React.FC<OptionProps> = ({
         <div
           className="fixed top-0 left-0 w-full h-full -z-10"
           onClick={() => setOpen(!open)}
+          onMouseOver={() => setOpen(!open)}
         />
       ) : (
         <></>
