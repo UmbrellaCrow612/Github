af868a8201709b455e7b49be9ce6c32e9281812d
Yousaf Wazir <107571370+UmbrellaCrow612@users.noreply.github.com>
2022-08-02 13:49:11 +0100

    feat: added redirect home (#38)
    * style: bg col added to header to span full screen
    
    * style: changed folder name
    
    * style: removed the sticky header
    
    * feat: redirecting un-auth user to home

--- src/components/Navigation/Authenticated/Authenticated.tsx
+++ src/components/Navigation/Authenticated/Authenticated.tsx
@@ -12,7 +12,7 @@ interface AuthenticatedProps {}
 export const Authenticated: React.FC<AuthenticatedProps> = () => {
   return (
     <header className="w-full bg-[#161B22]">
-      <nav className="max-w-screen-2xl mx-auto h-[85px] flex items-center justify-between px-3  md:h-[70px] sticky top-0 z-10">
+      <nav className="max-w-screen-2xl mx-auto h-[85px] flex items-center justify-between px-3  md:h-[70px]">
         <MobileMenu />
 
         {/** Left side */}
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>home</title>
+      </Head>
+    </div>
+  )
+}
+export default home
