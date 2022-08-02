3a25d4ed97dd070003619d113e4a90fe2befdaf8
Yousaf Wazir <107571370+UmbrellaCrow612@users.noreply.github.com>
2022-08-02 13:16:29 +0100

    style: bg col added to header to span full screen (#36)

--- src/components/Nav/UnAuthenticated/UnAuthenticated.tsx
+++ src/components/Nav/UnAuthenticated/UnAuthenticated.tsx
@@ -9,8 +9,8 @@ interface UnAuthenticatedProps {}
 
 export const UnAuthenticated: React.FC<UnAuthenticatedProps> = () => {
   return (
-    <header>
-      <nav className="max-w-screen-2xl mx-auto h-[85px] flex items-center justify-between px-3 bg-[#161B22] md:h-[70px] sticky top-0 z-10">
+    <header className="w-full bg-[#161B22]">
+      <nav className="max-w-screen-2xl mx-auto h-[85px] flex items-center justify-between px-3 md:h-[70px] sticky top-0 z-10">
         <div className="flex items-center p-2">
           <Logo />
           <HeaderOptions />
