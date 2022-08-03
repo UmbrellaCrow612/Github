8b2a8079355d2d4e2fe784aea3924b5897730111
yousaf wazir <umbrellaman612@gmail.com>
2022-08-03 09:30:28 +0100

    feat: added mobile menu links

--- src/components/Navigation/UnAuthenticated/UnAuthMobileOption.tsx
+++ src/components/Navigation/UnAuthenticated/UnAuthMobileOption.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { useEffect, useState } from 'react'
 import { MdExpandMore } from 'react-icons/md'
 
@@ -22,25 +24,27 @@ export const UnAuthMobileOption: React.FC<UnAuthMobileOptionProps> = ({
     setLoading(false)
   }, [])
 
-
-
   if (loading) return <h1>Loading</h1>
 
   return (
     <div className="relative">
-      <button
-        className="mb-1 w-full p-3 group cursor-pointer text-start  border-b-[0.5px] border-gray-700 flex items-center"
-        onClick={() => setOpen(!open)}
-      >
-        <h5 className="group-hover:text-[#575A62] text-[#d2d4d8] text-xl tracking-wide">
-          {title}
-        </h5>
-        {isDropDown ? (
-          <MdExpandMore className="group-hover:text-[#575A62] text-[#d2d4d8] text-xl" />
-        ) : (
-          <></>
-        )}
-      </button>
+      <Link href={`/${href}`} key={title}>
+        <a>
+          <button
+            className="mb-1 w-full p-3 group cursor-pointer text-start  border-b-[0.5px] border-gray-700 flex items-center"
+            onClick={() => setOpen(!open)}
+          >
+            <h5 className="group-hover:text-[#575A62] text-[#d2d4d8] text-xl tracking-wide">
+              {title}
+            </h5>
+            {isDropDown ? (
+              <MdExpandMore className="group-hover:text-[#575A62] text-[#d2d4d8] text-xl" />
+            ) : (
+              <></>
+            )}
+          </button>
+        </a>
+      </Link>
 
       {/** Is dropdown and will show menu */}
       {isDropDown ? (
@@ -53,16 +57,22 @@ export const UnAuthMobileOption: React.FC<UnAuthMobileOptionProps> = ({
           key={100}
         >
           {DropDownOptions?.map((Option) => (
-            <button
-              className={`w-full p-2 text-gray-300 text-start hover:text-gray-400 ${
-                Option.bold ? 'font-bold' : ''
-              } ${
-                Option.borderBottom ? 'border-b-[0.5px] border-gray-600' : ''
-              }`}
-              key={Option.title}
-            >
-              {Option.title}
-            </button>
+            <Link href={`/${Option.href}`} key={Option.title}>
+              <a>
+                <button
+                  className={`w-full p-2 text-gray-300 text-start hover:text-gray-400 ${
+                    Option.bold ? 'font-bold' : ''
+                  } ${
+                    Option.borderBottom
+                      ? 'border-b-[0.5px] border-gray-600'
+                      : ''
+                  }`}
+                  key={Option.title}
+                >
+                  {Option.title}
+                </button>
+              </a>
+            </Link>
           ))}
         </div>
       ) : (
@@ -78,7 +88,6 @@ export const UnAuthMobileOption: React.FC<UnAuthMobileOptionProps> = ({
       ) : (
         <></>
       )}
-
     </div>
   )
 }
